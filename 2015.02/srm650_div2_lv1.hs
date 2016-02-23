6b3503fbae4d16997ba5302a1422e7f05624939c
nolleh <nolleh7707@gmail.com>
2016-02-23 21:51:33 +0900

    650

--- 2015.02/srm650_div2_lv1.hs
+++ 2015.02/srm650_div2_lv1.hs
@@ -1,13 +1,19 @@
 get_number :: Int -> Int -> Int
-get_number x y = refine $ logBase 2 (fromIntegral $ gcd x y)
+get_number a b = min (get a) (get b) + (refine a b)
 
--- realfrac for compare with 0.0
-refine :: RealFrac a => a -> Int
-refine a
-  | a /= 0.0 && (a == (fromIntegral (ceiling a))) = floor (a + 1)
-  | otherwise = floor a
+get :: Int -> Int
+get a
+  | (rem a 2) /= 0 = 0
+  | otherwise = 1 + get (div a 2)
+
+refine :: Int -> Int -> Int
+refine a b
+  | (rem a b) == 0 || (rem b a) == 0 = 1
+  | otherwise = 0
 
 main = do
+-- 8 / 4, 2, 1
+-- 4 / 2, 1
   print $ get_number 8 4 -- 3
   print $ get_number 4 7 -- 0
   print $ get_number 12 12 -- 3
